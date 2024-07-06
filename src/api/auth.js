9a4e8b8fb13d6f5daaaf3fa6303aab8092b2d320
Brendan <brendanjlee5@gmail.com>
2024-07-06 17:18:23 +0900

    signup function

--- src/api/auth.js
+++ src/api/auth.js
@@ -26,6 +26,32 @@ const login = async (username, password) => {
   }
 };
 
+const signup = async (email, username, password) => {
+  try {
+    const { res } = await await axios.post(
+      `${apiBaseUrl}/auth/signup`,
+      {
+        email: email,
+        username: username,
+        password: password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return res;
+  } catch (error) {
+    if (error.response) {
+      throw error.response.data;
+    } else {
+      throw new Error("Network error");
+    }
+  }
+};
+
 export default {
   login,
+  signup,
 };
--- src/components/AuthPage/Signup.jsx
+++ src/components/AuthPage/Signup.jsx
@@ -7,14 +7,51 @@ import {
   Text,
   Center,
   Button,
+  Box,
+  Alert,
+  AlertIcon,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import util from "../../utils/validate";
+import auth from "../../api/auth";
 
 function SignUp() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setError("");
+
+    // validate input
+    if (!util.validateSingup(email, username, password)) {
+      setError(
+        "Username must be at least 5 characters and password must be at least 8 characters"
+      );
+      return;
+    }
+
+    try {
+      const res = await auth.signup(email, username, password);
+      console.log(res);
+
+      setSuccess(true);
+      navigate("/login");
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
   return (
     <Center mt={"10px"}>
       <Flex
         bg={"white"}
-        w={"55%"}
+        w={"30%"}
         flexDir={"column"}
         alignItems={"center"}
         gap={"5px"}
@@ -24,18 +61,54 @@ function SignUp() {
           Signup
         </Text>
         <Text>Create an account to create your own posts!</Text>
-        <FormControl p={"15px"} isRequired>
-          <FormLabel>Email</FormLabel>
-          <Input type={"email"} id="email" name="email" />
-          <FormLabel htmlFor="username">Username</FormLabel>
-          <Input type={"text"} id="username" name="username" />
-          <FormLabel htmlFor="password">Password</FormLabel>
-          <Input type={"password"} id="password" name="password"></Input>
-        </FormControl>
-        <Spacer />
-        <Button type="submit" formAction="submit">
-          Signup
-        </Button>
+        {error && (
+          <Box w="100%" maxW={"md"}>
+            <Alert status="error">
+              <AlertIcon />
+              {error}
+            </Alert>
+          </Box>
+        )}
+        {success && (
+          <Box w="100%" maxW={"md"}>
+            <Alert status="success">
+              <AlertIcon />
+              Login Success
+            </Alert>
+          </Box>
+        )}
+        <form onSubmit={handleSubmit}>
+          <FormControl p={"15px"} isRequired>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              type={"email"}
+              id="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+              autoComplete="off"
+            />
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <Input
+              type={"text"}
+              id="username"
+              name="username"
+              onChange={(e) => setUsername(e.target.value)}
+              autoComplete="off"
+            />
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              type={"password"}
+              id="password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="off"
+            ></Input>
+          </FormControl>
+          <Spacer />
+          <Button type="submit" formAction="submit">
+            Signup
+          </Button>
+        </form>
       </Flex>
     </Center>
   );
